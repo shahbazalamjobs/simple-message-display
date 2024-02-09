4c9949ed74bc703c5fe1707ffe2079f5eb4ff4dd
Shahbaz <alamshahbaz366@gmail.com>
2024-02-09 23:49:07 +0530

    update

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PGPORT || 5000;
 
 app.use(cors({
     origin: 'http://localhost:5173'
